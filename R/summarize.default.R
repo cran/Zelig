8c13bc9b70550bd97edca93d9359134489c2f07a
Kosuke Imai <kimai@Princeton.Edu>
2010-01-20 00:00:00 +0000

    version 3.5

--- R/summarize.default.R
+++ R/summarize.default.R
@@ -3,7 +3,7 @@ summarize.default <- function(x, rows = NULL, cip, stats, subset = NULL) {
   if (is.numeric(x)) {
     for (i in 1:length(stats))
       res <- c(res, do.call(stats[i], list(x)))
-    res <- c(res, quantile(x, cip))
+    res <- c(res, quantile(x, cip, na.rm=TRUE))
     names(res) <- c(stats, paste(cip*100, "%", sep = ""))
   }
   else if (is.character(x)) {
