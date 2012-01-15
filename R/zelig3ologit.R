04c8d92efe88e68bd4ffd11094a17d2b62355aa6
Kosuke Imai <kimai@Princeton.Edu>
2012-01-15 12:18:34 +0000

    version 3.5.3

--- R/zelig3ologit.R
+++ R/zelig3ologit.R
@@ -1,7 +1,7 @@
 zelig3ologit <- function(res, fcall = NULL, zcall = NULL) { 
   inv.link <- function(eta, zeta) {
     tmp1 <- matrix(1, nrow = length(eta), ncol = length(zeta) + 1)
-    ilogit <- function(e, z) {
+    ilogit <- function(z, e) {
       exp(z - e) / (1 + exp(z - e))
     }
     tmp1[, 1:length(zeta)] <- sapply(zeta, ilogit, e = eta)
