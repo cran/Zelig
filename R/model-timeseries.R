26c172a9874be7a93b7f7fe0f01812adf1214542
James Honaker <zelig-zee@iq.harvard.edu>
2016-04-10 17:34:00 +0000

    version 5.0-12

--- R/model-timeseries.R
+++ R/model-timeseries.R
@@ -36,20 +36,51 @@ ztimeseries$methods(
 )
 
 ztimeseries$methods(
-  zelig = function(formula, data, ..., weights=NULL, by=NULL, bootstrap = FALSE){
+  zelig = function(formula, data, order=c(1,0,0), ts=NULL, cs=NULL, ..., weights=NULL, by=NULL, bootstrap = FALSE){
     if(!identical(bootstrap,FALSE)){
       stop("Error: The bootstrap is not implemented for time-series models")
     }
+    .self$zelig.call <- match.call(expand.dots = TRUE)
     if(identical(.self$name,"ar")){
       order<-c(1,0,0)
+      .self$zelig.call$order <- order
     } else if(identical(.self$name,"ma")){
       order<-c(0,0,1)
+      .self$zelig.call$order <- order
     }
-    .self$zelig.call <- match.call(expand.dots = TRUE)
     .self$model.call <- .self$zelig.call
-    callSuper(formula = formula, data = data, ..., weights = weights, by = by, bootstrap = FALSE)
+
+    ## Sort dataset by time and cross-section
+    ## Should add checks that ts, cs, are valid, and consider how to interact with by.
+    ## This follows handling from Amelia::prep.r, which also has code to deal with lags, should we add those.
+    if(!identical(ts,NULL)){
+      .self$model.call$ts <- NULL
+      if (!identical(cs,NULL)) {
+        .self$model.call$cs <- NULL
+        tsarg<-list(data[,cs],data[,ts])
+        by <- cs  # Use by architecture to deal with cross-sections in time-series models that do not support such.  Currently overrides.
+      } else {
+        tsarg<-list(data[,ts])
+      }
+
+      tssort<-do.call("order",tsarg)
+      data<-data[tssort,]
+    }
+
+    ## ts and cs are used to reorganize dataset, and do not get further passed on to Super
+    callSuper(formula = formula, data = data, order=order, ..., weights = weights, by = by, bootstrap = FALSE)
   }
 )
+
+# replace packagename method as stats::arima() has a second layer of wrapping in zeligArimaWrapper().
+
+ztimeseries$methods(
+  packagename = function() {
+    "Automatically retrieve wrapped package name"
+    return("stats")
+  }
+)
+
 
 # replace simx method to add ACF as QI.
 
@@ -106,3 +137,16 @@ ztimeseries$methods(
     }
   }
 )
+
+# There is no fitting summary function for objects of class Arima.  
+# So this passes the object through to print, and z$summary() is essentially print(summary(x)).
+
+#' Summary of an object of class Arima
+#' @method summary Arima
+#' @param object An object of class Arima 
+#' @param ... Additional parameters
+#' @return The original object 
+#' @export
+
+
+summary.Arima = function(object, ...) object
